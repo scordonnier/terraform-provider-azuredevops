d24be32ba50bc57ac46fda79b9acab1ae9ba1806
Stephane Cordonnier <scordonnier@beecomedigital.com>
2022-12-30 12:22:56 +0100

    Initialize DistributedTaskClient to manage pipeline environments

---
+++ internal/clients/distributedtask/client.go
@@ -0,0 +1,73 @@
+package distributedtask
+
+import (
+	"context"
+	"github.com/scordonnier/terraform-provider-azuredevops/internal/networking"
+	"strconv"
+)
+
+const (
+	pathApis            = "_apis"
+	pathDistributedTask = "distributedtask"
+	pathEnvironments    = "environments"
+)
+
+type Client struct {
+	restClient *networking.RestClient
+}
+
+func NewClient(restClient *networking.RestClient) *Client {
+	return &Client{
+		restClient: restClient,
+	}
+}
+
+func (c *Client) CreateEnvironment(ctx context.Context, projectId string, name string, description string) (*EnvironmentInstance, error) {
+	pathSegments := []string{projectId, pathApis, pathDistributedTask, pathEnvironments}
+	body := &CreateOrUpdateEnvironmentArgs{
+		Description: &description,
+		Name:        &name,
+	}
+	resp, err := c.restClient.PostJSON(ctx, pathSegments, nil, body, networking.ApiVersion70)
+	if err != nil {
+		return nil, err
+	}
+
+	var environment *EnvironmentInstance
+	err = c.restClient.ParseJSON(ctx, resp, &environment)
+	return environment, err
+}
+
+func (c *Client) DeleteEnvironment(ctx context.Context, projectId string, id int) error {
+	pathSegments := []string{projectId, pathApis, pathDistributedTask, pathEnvironments, strconv.Itoa(id)}
+	_, err := c.restClient.DeleteJSON(ctx, pathSegments, nil, networking.ApiVersion70)
+	return err
+}
+
+func (c *Client) GetEnvironment(ctx context.Context, projectId string, id int) (*EnvironmentInstance, error) {
+	pathSegments := []string{projectId, pathApis, pathDistributedTask, pathEnvironments, strconv.Itoa(id)}
+	resp, err := c.restClient.GetJSON(ctx, pathSegments, nil, networking.ApiVersion70)
+	if err != nil {
+		return nil, err
+	}
+
+	var environment *EnvironmentInstance
+	err = c.restClient.ParseJSON(ctx, resp, &environment)
+	return environment, err
+}
+
+func (c *Client) UpdateEnvironment(ctx context.Context, projectId string, id int, name string, description string) (*EnvironmentInstance, error) {
+	pathSegments := []string{projectId, pathApis, pathDistributedTask, pathEnvironments, strconv.Itoa(id)}
+	body := &CreateOrUpdateEnvironmentArgs{
+		Description: &description,
+		Name:        &name,
+	}
+	resp, err := c.restClient.PatchJSON(ctx, pathSegments, nil, body, networking.ApiVersion70)
+	if err != nil {
+		return nil, err
+	}
+
+	var environment *EnvironmentInstance
+	err = c.restClient.ParseJSON(ctx, resp, &environment)
+	return environment, err
+}
---
+++ internal/clients/distributedtask/models.go
@@ -0,0 +1,31 @@
+package distributedtask
+
+import (
+	"github.com/scordonnier/terraform-provider-azuredevops/internal/clients/core"
+)
+
+type CreateOrUpdateEnvironmentArgs struct {
+	Description *string `json:"description,omitempty"`
+	Name        *string `json:"name,omitempty"`
+}
+
+type EnvironmentInstance struct {
+	CreatedBy      *core.IdentityRef               `json:"createdBy,omitempty"`
+	CreatedOn      *core.Time                      `json:"createdOn,omitempty"`
+	Description    *string                         `json:"description,omitempty"`
+	Id             *int                            `json:"id,omitempty"`
+	LastModifiedBy *core.IdentityRef               `json:"lastModifiedBy,omitempty"`
+	LastModifiedOn *core.Time                      `json:"lastModifiedOn,omitempty"`
+	Name           *string                         `json:"name,omitempty"`
+	Project        *core.ProjectReference          `json:"project,omitempty"`
+	Resources      *[]EnvironmentResourceReference `json:"resources,omitempty"`
+}
+
+type EnvironmentResourceReference struct {
+	Id   *int                     `json:"id,omitempty"`
+	Name *string                  `json:"name,omitempty"`
+	Tags *[]string                `json:"tags,omitempty"`
+	Type *EnvironmentResourceType `json:"type,omitempty"`
+}
+
+type EnvironmentResourceType string
